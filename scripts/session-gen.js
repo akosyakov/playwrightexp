d9ba4ba87961d5c1fcbb003fd4b7f54b73a1b31e
mustard <mhqnwt@gmail.com>
2023-12-07 01:32:01 +0800

    Proper logging

--- scripts/session-gen.js
+++ scripts/session-gen.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
+const { writeFileSync } = require('node:fs')
+const { spawn } = require('node:child_process')
 
 const { GITPOD_HOST, GITPOD_AUTH_SESSION } = process.env;
+
+if (!GITPOD_HOST || !GITPOD_AUTH_SESSION) {
+    writeFileSync(__dirname + "/../.env", `GITPOD_HOST=gitpod.io\nGITPOD_AUTH_SESSION=.auth/io_session.json\n`);
+    throw new Error('GITPOD_HOST and GITPOD_AUTH_SESSION must be set. Check your .env file');
+}
 
 if (!GITPOD_AUTH_SESSION.startsWith('.auth/')) {
     throw new Error('GITPOD_AUTH_SESSION must start with .auth/');
 }
 
-if (!GITPOD_HOST || GITPOD_HOST.startsWith('https://')) {
-    throw new Error('GITPOD_HOST must be set to the host of your Gitpod instance, e.g. gitpod.io');
+if (GITPOD_HOST.startsWith('https://')) {
+    throw new Error('GITPOD_HOST must be set to the host of your Gitpod Installation, e.g. gitpod.io');
 }
 
-const { exec, spawn } = require('node:child_process')
+const logYellow = (str) => {
+    console.log('\x1b[33m' + str + '\x1b[0m');
+}
+
+logYellow('\x1b[33mDisable recording and log into your Gitpod Installation\x1b[0m');
 
 spawn('npx', [
     'playwright',
